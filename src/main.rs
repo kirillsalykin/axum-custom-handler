41551bde23bd1b214dfd5fb9a97151487b635ee2
Kirill Salykin <kirill.salykin@gmail.com>
2025-03-28 19:54:32 +0100

    Kinda works

--- src/main.rs
+++ src/main.rs
@@ -8,69 +8,32 @@ use axum::{
     routing::post,
 };
 
-use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
+use specta::{Type, ts};
 use std::{fmt::Debug, future::Future};
 use std::{marker::PhantomData, pin::Pin};
 
 #[derive(Clone)]
-pub struct ApiHandler<F, Extractors, Input, Output> {
+pub struct ApiHandler<F, Input, Output> {
     inner: F,
-    _marker: PhantomData<(Extractors, Input, Output)>,
+    _marker: PhantomData<(Input, Output)>,
 }
 
-pub trait IntoApiHandler<Extractors, Input, Output> {
+pub trait IntoApiHandler<Input, Output> {
     type Handler;
     fn into_api_handler(self) -> Self::Handler;
 }
 
-impl<F, Fut, Output> IntoApiHandler<(), (), Output> for F
-where
-    F: FnOnce() -> Fut + Clone + Send + Sync + 'static,
-    Fut: Future<Output = ApiResult<Output>> + Send + 'static,
-    Output: Serialize + Clone + Send + Sync + 'static,
-{
-    type Handler = ApiHandler<F, (), (), Output>;
-    fn into_api_handler(self) -> Self::Handler {
-        ApiHandler {
-            inner: self,
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<F, Fut, S, Output> Handler<(), S> for ApiHandler<F, (), (), Output>
-where
-    F: FnOnce() -> Fut + Clone + Send + Sync + 'static,
-    Fut: Future<Output = ApiResult<Output>> + Send + 'static,
-    Output: Serialize + Clone + Send + Sync + 'static,
-    S: Send + Sync + 'static,
-{
-    type Future = Pin<Box<dyn Future<Output = Response> + Send>>;
-
-    fn call(self, _req: Request<axum::body::Body>, _state: S) -> Self::Future {
-        Box::pin(async move {
-            match (self.inner)().await {
-                Ok(output) => Json::<Output>(output).into_response(),
-                Err(err) => err.into_response(),
-            }
-        })
-    }
-}
-
-macro_rules! impl_api_handler {
-    (
-        [$($ty:ident),*], $Input:ident
-    ) => {
-        impl<F, Fut, $($ty,)* $Input, Output> IntoApiHandler<( $($ty,)* ), $Input, Output>
+macro_rules! impl_iah {
+  ([$($ty:ident),* $(,)?]) => {
+        impl<F, Fut, $($ty,)* Output> IntoApiHandler<( $($ty,)* ), Output>
         for F
         where
-            F: FnOnce( $($ty,)* $Input ) -> Fut + Clone + Send + Sync + 'static,
+            F: FnOnce( $($ty,)* ) -> Fut + Clone + Send + Sync + 'static,
             Fut: Future<Output = ApiResult<Output>> + Send + 'static,
-            $Input: DeserializeOwned + Clone + Send + Sync + 'static,
             Output: Serialize + Clone + Send + Sync + 'static,
         {
-            type Handler = ApiHandler<F, ( $( $ty, )* ), $Input, Output>;
+            type Handler = ApiHandler<F, ( $( $ty, )* ), Output>;
 
             fn into_api_handler(self) -> Self::Handler {
                 ApiHandler {
@@ -79,38 +42,71 @@ macro_rules! impl_api_handler {
                 }
             }
         }
+    }
+}
 
-        #[allow(non_snake_case, unused_mut)]
-        impl<F, Fut, S, $($ty,)* $Input, Output> Handler<( $($ty,)* $Input, ), S>
-        for ApiHandler<F, ( $($ty,)* ), $Input, Output>
+impl_iah!([]);
+impl_iah!([T1]);
+impl_iah!([T1, T2]);
+
+macro_rules! impl_into_api_handler {
+    ([$($ty:ident),* $(,)?]) => {
+        impl<F, Fut, $($ty,)* Output> From<F> for ApiHandler<F, ( $($ty,)* ), Output>
         where
-            F: FnOnce( $( $ty, )* $Input, ) -> Fut + Clone + Send + Sync + 'static,
+            F: FnOnce( $($ty,)* ) -> Fut + Clone + Send + Sync + 'static,
             Fut: Future<Output = ApiResult<Output>> + Send + 'static,
-            $( $ty: FromRequestParts<S> + Clone + Send + Sync + 'static, )*
-            $Input: DeserializeOwned + Clone + Send + Sync + 'static,
-            Output: Serialize + Clone + Send + Sync + 'static,
+            Output: Clone + Send + Sync + 'static,
+        {
+            fn from(f: F) -> Self {
+                ApiHandler {
+                    inner: f,
+                    _marker: PhantomData,
+                }
+            }
+        }
+    }
+}
+
+impl_into_api_handler!([]);
+impl_into_api_handler!([T1]);
+impl_into_api_handler!([T1, T2]);
+
+macro_rules! impl_handler {
+    (
+        [$($ty:ident),*], $last:ident
+    ) => {
+        #[allow(non_snake_case, unused_mut)]
+        impl<F, Fut, S, $($ty,)* $last, Output> Handler<(Output, $($ty,)* $last, ), S>
+        for ApiHandler<F, ( $($ty,)* $last,), Output>
+        where
+            F: FnOnce( $( $ty, )* $last, ) -> Fut + Clone + Send + Sync + 'static,
+            Fut: Future<Output = ApiResult<Output>> + Send,
             S: Send + Sync + 'static,
+            $( $ty: FromRequestParts<S> + Clone + Send + Sync + 'static, )*
+            $last: FromRequest<S> + Clone + Send + Sync + 'static,
+            Output: Serialize + Clone + Send + Sync + 'static,
         {
             type Future = Pin<Box<dyn Future<Output = Response> + Send>>;
 
             fn call(self, req: Request<axum::body::Body>, state: S) -> Self::Future {
-                Box::pin(async move {
-                    let (mut parts, body) = req.into_parts();
-                    let state = &state;
+                let (mut parts, body) = req.into_parts();
 
+                Box::pin(async move {
                     $(
-                        let $ty = match $ty::from_request_parts(&mut parts, state).await {
+                        let $ty = match $ty::from_request_parts(&mut parts, &state).await {
                             Ok(value) => value,
                             Err(rejection) => return rejection.into_response(),
                         };
                     )*
 
-                    let Json(input) = match Json::<$Input>::from_request(Request::from_parts(parts, body), state).await {
+                    let req = Request::from_parts(parts, body);
+
+                    let $last = match $last::from_request(req, &state).await {
                         Ok(value) => value,
                         Err(rejection) => return rejection.into_response(),
                     };
 
-                    match (self.inner)($($ty,)* input).await {
+                    match (self.inner)($($ty,)* $last).await {
                       Ok(output) => Json::<Output>(output).into_response(),
                       Err(err) => err.into_response()
                     }
@@ -120,22 +116,27 @@ macro_rules! impl_api_handler {
     }
 }
 
-//impl_api_handler!([], T1);
-impl_api_handler!([T1], T2);
-impl_api_handler!([T1, T2], T3);
+impl_handler!([], T1);
+impl_handler!([T1], T2);
+impl_handler!([T1, T2], T3);
 
 ///
 ///
 ///
 ///
 
-async fn handler(h: HeaderMap, input: Input) -> ApiResult<Output> {
-    //Err(ApiError::InternalError)
-    Ok(Output { field: input.field })
+async fn handler(_h: HeaderMap, input: Json<Input>) -> ApiResult<Output> {
+    println!("IWASCALLED");
+    Ok(Output {
+        field: input.0.field,
+    })
 }
 
 async fn empty() -> ApiResult<Output> {
-    //Err(ApiError::InternalError)
+    Ok(Output { field: "".into() })
+}
+
+async fn empty1(_h: HeaderMap) -> ApiResult<Output> {
     Ok(Output { field: "".into() })
 }
 
@@ -152,21 +153,59 @@ impl IntoResponse for ApiError {
 
 type ApiResult<T> = Result<T, ApiError>;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Type)]
 struct Input {
     field: String,
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Type)]
 struct Output {
     field: String,
 }
 
+pub struct ApiBuilder<S> {
+    router: Router<S>,
+}
+
+impl<S> ApiBuilder<S>
+where
+    S: Clone + Send + Sync + 'static,
+{
+    pub fn new() -> Self {
+        Self {
+            router: Router::<S>::new(),
+        }
+    }
+
+    pub fn add<H, T: 'static, Input, Output>(mut self, name: &str, handler: H) -> Self
+    where
+        H: IntoApiHandler<Input, Output>,
+        H::Handler: Handler<T, S>,
+    {
+        self.router = self
+            .router
+            .route(&format!("/{}", name), post(handler.into_api_handler()));
+        self
+    }
+
+    pub fn build(self) -> Router<S> {
+        self.router
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", post(handler.into_api_handler()))
-        .route("/", post(empty.into_api_handler()));
+    let api = ApiBuilder::new()
+        //.add("empty1", empty1)
+        .add("handler", handler)
+        .build();
+
+    let app = Router::new().merge(api);
+
+    //    //.route("empty", post(empty.into_api_handler()))
+    //    //.route("empty1", post(empty1.into()))
+    //    //.route("handler", post(handler.into())
+    //.route("handler", post(handler.into_api_handler()));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
